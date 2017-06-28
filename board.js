600d60a47c70ec3b322b35d2940e5b5e29a770bc
Ethan Arns <narkro555@gmail.com>
2017-06-28 16:32:22 -0400

    Initial upload

---
+++ board.js
@@ -0,0 +1,54 @@
+'use strict';
+class SudokuBoard {
+  constructor() {
+    // Create board filled with 0s
+    this.filled = 0;
+    this.content = new Array(9);
+    for(let x = 0; x < 9; x++) {
+      this.content[x] = new Array(9);
+      for(let y = 0; y < 9; y++) {
+        this.content[x][y] = 0;
+      }
+    }
+  }
+
+  get(x,y) {
+    if(!Number.isInteger(x) || !Number.isInteger(y)) {
+      console.log("ERROR: get() INVALID INPUT");
+      return -1
+    }
+    if(x < 1 || x > 9 || y < 1 || y > 9) {
+      console.log("ERROR: get() OUT OF BOUNDS");
+      return -1;
+    }
+    return this.content[y-1][x-1];
+  }
+
+  set(x,y,val) {
+    if(!Number.isInteger(x) || !Number.isInteger(y) || !Number.isInteger(val)) {
+      console.log("ERROR: set() INVALID INPUT");
+      return -1
+    }
+    if(x < 1 || x > 9 || y < 1 || y > 9) {
+      console.log("ERROR: set() OUT OF BOUNDS");
+      return -1;
+    }
+    this.content[y-1][x-1] = val;
+  }
+
+  printBoard() {
+    for(let x = 1; x <= 9; x++) {
+      var row = [];
+      for(let y = 1; y <= 9; y++) {
+        row.push(this.get(y,x))
+      }
+      console.log(row);
+    }
+  }
+}
+
+var board = new SudokuBoard();
+board.set(3,4,9);
+board.set(4,3,8);
+console.log(board.get(3,4));
+board.printBoard();
---
+++ main.js
@@ -0,0 +1,4 @@
+// console.log("fuck");
+// var board2 = new SudokuBoard();
+// board2.set(3,4,9);
+// console.log(board2.get(3,4));
