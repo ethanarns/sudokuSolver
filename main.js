5a832b1139d3c0c2f17312d796fcdba100386dd4
Ethan Arns <narkro555@gmail.com>
2017-07-24 18:00:54 -0700

    Input and styling completed

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var board2 = new SudokuBoard();
 board2.set(3,4, 9);
 console.log(board2.get(3,4));
 
-// Construct the table
-
+// Construct the HTML table
 var div4table = document.getElementById("board_array");
 var initialTable = document.createElement("table");
 initialTable.className = "board_table";
@@ -18,7 +17,24 @@ for(let i = 0; i < 9; i++) {
     var tempCell = document.createElement("td");
     tempCell.className = "tableCell";
     tempCell.id = "cell-" + (j+1) + "-" + (i+1);
+    var tempInput = document.createElement("input");
+    tempInput.type = "text";
+    tempInput.name = "numberInput";
+    tempInput.id = "input-" + (j+1) + "-" + (i+1);
+    tempInput.value = "0";
+    tempInput.style.maxlength = "1";
+    tempCell.appendChild(tempInput);
+    if(j == 3 || j == 6) {
+      tempCell.style.borderWidth = "1px 1px 1px 5px";
+    }
+    if(i == 3 || i == 6) {
+      tempCell.style.borderWidth = "5px 1px 1px 1px";
+    }
+    if( (i == 3 || i == 6) && (j == 3 || j == 6) ) {
+      tempCell.style.borderWidth = "5px 1px 1px 5px";
+    }
     tempRow.appendChild(tempCell);
   }
   initialTable.appendChild(tempRow);
 }
+//
