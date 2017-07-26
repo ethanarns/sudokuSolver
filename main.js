855093189cda05e087a02d28bd0f7cf726ba46a7
Ethan Arns <narkro555@gmail.com>
2017-07-26 11:02:16 -0700

    Unsolved version test added

--- main.js
+++ main.js
@@ -309,3 +309,11 @@ function solve_recurse(numPos, num2test) {
  */
 
 var board = new SudokuBoard();
+board.set(1,1, 3);board.set(3,1, 5);board.set(4,1, 6);board.set(5,1, 4);
+board.set(6,1, 8);board.set(8,1, 1);board.set(1,2, 2);board.set(2,2, 1);
+board.set(8,2, 4);board.set(5,3, 2);board.set(2,4, 3);board.set(4,4, 8);
+board.set(9,4, 6);board.set(1,5, 1);board.set(2,5, 9);board.set(4,5, 5);
+board.set(6,5, 6);board.set(8,5, 7);board.set(3,6, 4);board.set(7,6, 8);
+board.set(8,6, 9);board.set(4,7, 7);board.set(7,7, 4);board.set(2,8, 4);
+board.set(5,8, 1);board.set(6,8, 9);board.set(2,9, 5);board.set(3,9, 3);
+board.set(6,9, 2);board.set(7,9, 7);
